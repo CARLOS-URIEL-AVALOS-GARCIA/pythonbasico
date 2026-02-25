f3c2c22578439bd5b5b0a5f27e6bc645c8e3a5cc
CARLOS-URIEL-AVALOS-GARCIA <avalosambrocio2@gmail.com>
2026-02-24 17:02:31 -0800

    usamos el enemigo

---
+++ OOP/Enemigo.py
@@ -0,0 +1,21 @@
+class Enemigo:
+    tipo_Enemigo:str
+    puntos_de_energia: int = 10
+    ataque = 1
+
+    def _int_(self,tipo_enemigo, puntos_energia=10, ataque=1):
+        self.__tipo_enemigo = tipo_enemigo
+        self.puntos_energia = puntos_energia
+        self.ataque = ataque
+
+        def get_tipo_enemigo(self):
+            return self.__tipo_enemigo
+        
+        def habla(self):
+            print(f"yo soy {self.__tipo_Enemigo}. preparando para pelear")
+
+            def camina(self):
+                print(f"{self.__tipo_enemigo} se mueve cerca de ti")
+                
+                def atacar(self):
+                    print(f"{self.__tipo_Enemigo} ataca con un {self.ataque} de daño")
